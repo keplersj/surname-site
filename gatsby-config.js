c20b5b78b53bec15bf200e714248faf5ff0422a8
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-08-26 01:48:49 -0600

    Take advantage of Gatsby setup and use some plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,12 @@ module.exports = {
   siteMetadata: {
     title: "Sticka-Jones Website",
     description: "",
-    author: "Kepler Sticka-Jones"
+    author: "Kepler Sticka-Jones",
+    siteUrl: "https://stickajones.org"
   },
   plugins: [
+    "gatsby-plugin-robots-txt",
+    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-mdx",
       options: {
@@ -13,6 +16,26 @@ module.exports = {
         }
       }
     },
-    "gatsby-plugin-react-helmet"
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-146396429-1",
+        anonymize: true,
+        respectDNT: true
+      }
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Sticka-Jones Website",
+        short_name: "Sticka-Jones",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        display: "minimal-ui"
+      }
+    },
+    "gatsby-plugin-offline"
   ]
 };
